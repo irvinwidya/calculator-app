055c454e28ef6ba2b4e5987d345689498262082f
Irvin Widyastanto <irvinwidya@gmail.com>
2024-04-19 02:35:25 +0700

    v0.1.1 - Optimizations and bug fixes

--- README.md
+++ README.md
@@ -1,17 +1,43 @@
 # Calculator App
 
-A simple calculator app built with React + MathJS library.
+A simple calculator app built with ReactJS and MathJS library.
 
-P.S. Known bugs when displaying proper number, such as leading zero(s) on operand. Maybe there's more. O_O)"
+> `Known Issues:`
+>
+> - None, yet ;p
 
 **Changelog:**
 
+<details>
+  <summary markdown="span">
+    v0.1.1 - Optimizations and bug fixes
+  </summary>
+
+> `Optimizations:`
+>
+> - Optimize key input logic
+> - Optimize input value and type detection
+> - Optimize display value handling
+>
+> `Fixes:`
+>
+> - Fix an issue where it's possible to input leading zeros on operand
+>   - _Example: **01**, **002+05**, etc._
+> - Fix an issue where it's possible to input operator side-by-side to another operator after using backspace
+>   - _Example: **2+-**, **3+1/+**, etc._
+
+</details>
+
 <details markdown="span">
-  <summary markdown="span">v0.1.0 - Published with limited functionality</summary>
-  
-  - Basic arithmetic operation (addition, substraction, multiplication, and division)
-  - Can only do a single equation
-  - Fractional number input is not supported
-  - Decimal result displayed only up to 8 digits
-  
+  <summary markdown="span">
+    v0.1.0 - Working calculator with limited functionality
+  </summary>
+
+> `Changes:`
+>
+> - Adds basic arithmetic operation (addition, substraction, multiplication, and division)
+> - Capable to do a single equation only
+> - Fractional number input is not supported yet
+> - Decimal result only displayed up to 8 digits
+
 </details>
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useRef } from "react";
-import { evaluate } from "mathjs";
+import { useRef } from "react"
+import { evaluate } from "mathjs"
 import {
   FaBackspace,
   FaDivide,
@@ -8,225 +8,284 @@ import {
   // FaPercentage,
   FaPlus,
   FaTimes,
-} from "react-icons/fa";
-import "./App.scss";
-import Info from "./Info";
+} from "react-icons/fa"
+import "./App.scss"
+// import Info from "./components/Info";
 
 export default function App() {
-  const inputDisplayRef = useRef();
-  const resultDisplayRef = useRef();
+  const inputDisplayRef = useRef()
+  const resultDisplayRef = useRef()
 
-  let inputDisplayEl, resultDisplayEl;
+  const operator = ["+", "-", "*", "/"]
+  const operand = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-  // let isOperandHasComma = false;
-  let currentOperator = "";
-  let inputValue = "0";
-  let resultValue = "0";
-  let answer = null;
+  let inputDisplayEl = null
+  let resultDisplayEl = null
+
+  let lastDigitValue = null
+  let lastDigitType = null
+  let lastTwoDigitValue = null
+
+  let buttonPressed = ""
+
+  let inputValue = "0"
+  let resultValue = "0"
+
+  let isOperatorUsed = false
+
+  let answer = null
 
   // let lastInput = "";
   // let inputValue_lastDigit = "";
 
-  function syncDisplayedValue() {
-    inputDisplayEl = inputDisplayRef.current;
-    resultDisplayEl = resultDisplayRef.current;
-    inputDisplayEl.innerHTML = inputValue;
-    resultDisplayEl.innerHTML = resultValue;
+  function focusToInput() {
+    handleDisplayValue()
+
+    inputDisplayEl.classList.add("focus")
+    resultDisplayEl.classList.remove("focus")
   }
 
-  function resetHighlight() {
-    inputDisplayEl = inputDisplayRef.current;
-    resultDisplayEl = resultDisplayRef.current;
-
-    inputDisplayEl.classList.add("highlight");
-    resultDisplayEl.classList.remove("highlight");
+  function handleDisplayValue() {
+    // Get the value of displayed input and result
+    inputDisplayEl = inputDisplayRef.current
+    resultDisplayEl = resultDisplayRef.current
+    // Update the value
+    inputDisplayEl.innerHTML = inputValue
+    resultDisplayEl.innerHTML = resultValue
   }
 
-  function inputInitialDigit(val) {
-    if (val === "+" || val === "-" || val === "*" || val === "/") {
-      currentOperator = val;
-      inputValue = "0" + val;
-      return;
+  function checkLastDigit() {
+    // Get the last digit of displayed input
+    lastDigitValue = inputValue.slice(-1)
+
+    // Check the last digit type between operand and operator
+    if (operator.includes(lastDigitValue)) {
+      lastDigitType = "operator"
+    } else {
+      lastDigitType = "operand"
     }
-    // if (val === ".") {
-    //   isOperandHasComma = true;
-    //   inputValue = "0" + val;
-    //   return;
-    // }
-    inputValue = val;
   }
 
-  function inputNextDigit(val) {
-    if (val === "+" || val === "-" || val === "*" || val === "/") {
-      if (currentOperator !== "") {
-        inputValue = inputValue.slice(0, -1);
+  function checkLastTwoDigit() {
+    // Get the last two digit of displayed input
+    lastTwoDigitValue = inputValue.slice(-2)
+    const firstDigit = lastTwoDigitValue.slice(0, 1)
+    const secondDigit = lastTwoDigitValue.slice(-1)
+
+    return { firstDigit, secondDigit }
+  }
+
+  function initialInput(val) {
+    if (val === "0") {
+      return
+    }
+
+    if (operator.includes(val)) {
+      inputValue = "0" + val
+      return
+    }
+    inputValue = val
+  }
+
+  function nextInput(val) {
+    if (lastDigitType === "operand") {
+      // could add another operand
+      // could add an operator
+      // could just show result
+
+      // To avoid leading zeros after an operator is being used
+      if (isOperatorUsed) {
+        let firstDigit = checkLastTwoDigit().firstDigit
+        let secondDigit = checkLastTwoDigit().secondDigit
+        // Ignore any operand key, if there is an operator followed with number 0
+        if (
+          operator.includes(firstDigit) &&
+          secondDigit === "0" &&
+          operand.includes(val)
+        ) {
+          return
+        }
       }
-      currentOperator = val;
+
+      inputValue = inputValue.concat(val)
+      return
     }
-    if (!isNaN(val)) {
-      currentOperator = "";
+    if (lastDigitType === "operator") {
+      // cannot add another operator
+      // could add operand
+      // could just show result
+
+      if (operator.includes(val)) {
+        inputValue = inputValue.slice(0, -1)
+      }
+      inputValue = inputValue.concat(val)
+      return
     }
-    inputValue = inputValue.concat(val);
   }
 
   function input(val) {
-    // lastInput = val;
+    focusToInput() // Focus back to input row after showing result
 
-    if (answer !== null) {
-      resetHighlight();
+    checkLastDigit()
+
+    // Check if at least one operator is being used
+    if (operator.includes(val)) {
+      isOperatorUsed = true
     }
 
+    // Check state
     if (inputValue === "0") {
-      inputInitialDigit(val);
-      syncDisplayedValue();
+      // Initial state
+      initialInput(val)
     } else {
-      inputNextDigit(val);
-      syncDisplayedValue();
+      // After initial state
+      nextInput(val)
     }
 
-    syncDisplayedValue();
-
-    // console.table({
-    //   val,
-    //   currentOperator,
-    //   inputValue,
-    //   answer,
-    // });
+    handleDisplayValue()
   }
 
-  // function checkLastDigitOfInputValue() {
-  //   inputValue_lastDigit = inputValue.slice(-1);
-  //   if (
-  //     inputValue_lastDigit === "+" ||
-  //     inputValue_lastDigit === "-" ||
-  //     inputValue_lastDigit === "*" ||
-  //     inputValue_lastDigit === "/"
-  //   ) {
-  //     currentOperator = lastInput;
-  //   }
-  //   if (!isNaN(inputValue_lastDigit)) {
-  //     currentOperator = "";
-  //   }
-  // }
-
   function backspace() {
-    if (inputValue !== "0") {
-      inputValue = inputValue.slice(0, -1);
-      if (inputValue === "") {
-        clr();
-        return;
-      }
+    if (inputValue.length !== 1) {
+      inputValue = inputValue.slice(0, -1)
+    } else if (inputValue.length === 1) {
+      inputValue = "0"
     }
-    syncDisplayedValue();
+
+    focusToInput()
+    handleDisplayValue()
   }
 
   function clr() {
-    resetHighlight();
-
-    inputValue = "0";
-    resultValue = "0";
-    currentOperator = "";
-    // isOperandHasComma = false;
-
-    syncDisplayedValue();
+    focusToInput()
+    resetAllValue()
+    handleDisplayValue()
   }
 
   function calculate() {
-    answer = evaluate(inputValue);
-    resultValue = parseFloat(answer.toFixed(8));
-    syncDisplayedValue();
+    answer = evaluate(inputValue)
+    resultValue = parseFloat(answer.toFixed(8))
   }
 
   function showResult() {
-    inputDisplayEl = inputDisplayRef.current;
-    resultDisplayEl = resultDisplayRef.current;
+    // When result is shown and the equal button is pressed, do nothing
+    if (inputValue === "0" && answer !== null) {
+      return
+    }
 
-    inputDisplayEl.classList.remove("highlight");
-    resultDisplayEl.classList.add("highlight");
+    inputDisplayEl = inputDisplayRef.current
+    resultDisplayEl = resultDisplayRef.current
 
-    calculate();
-    syncDisplayedValue();
+    inputDisplayEl.classList.remove("focus")
+    resultDisplayEl.classList.add("focus")
 
-    // Reset value
-    inputValue = "0";
-    resultValue = "0";
+    try {
+      calculate()
+    } catch (error) {
+      console.log(error)
+      resultValue = "Error"
+    } finally {
+      handleDisplayValue()
+    }
+
+    resetAllValue()
   }
 
-  console.log("Welcome to console!");
+  function resetAllValue() {
+    inputValue = "0"
+    resultValue = "0"
+    isOperatorUsed = false
+  }
+
+  console.log("Welcome to console!")
 
   return (
     <>
-      <main id="Calculator">
-        <Info />
+      <main id="Calculator" className="">
         <div id="Display">
-          <div ref={inputDisplayRef} id="InputRow" className="row highlight">
+          <div ref={inputDisplayRef} id="Display__Input" className="row focus">
             {inputValue}
           </div>
-          <div ref={resultDisplayRef} id="ResultRow" className="row grey">
+          <div
+            ref={resultDisplayRef}
+            id="Display__Result"
+            className="row inactive-color"
+          >
             {resultValue}
           </div>
         </div>
-        <div id="Keypad">
-          <button className="theme-color" onClick={() => clr()}>
-            C
+        <div id="Keypad" className="">
+          <button className="hover" onClick={() => clr()}>
+            <span className="theme-color">C</span>
           </button>
-          <button className="theme-color" onClick={() => backspace()}>
-            <FaBackspace />
+          <button className="hover" onClick={() => backspace()}>
+            <span className="theme-color">
+              <FaBackspace />
+            </span>
           </button>
-          <button className="theme-color no-hover">
-            {/* <FaPercentage /> */}
+          <button className="">{/* <FaPercentage /> */}</button>
+          <button className="hover" onClick={() => input("/")}>
+            <span className="theme-color">
+              <FaDivide />
+            </span>
           </button>
-          <button className="theme-color" onClick={() => input("/")}>
-            <FaDivide />
-          </button>
-          <button onClick={() => input("7")}>
+          <button className="hover" onClick={() => input("7")}>
             <span className="primary-color">7</span>
           </button>
-          <button onClick={() => input("8")}>
+          <button className="hover" onClick={() => input("8")}>
             <span className="primary-color">8</span>
           </button>
-          <button onClick={() => input("9")}>
+          <button className="hover" onClick={() => input("9")}>
             <span className="primary-color">9</span>
           </button>
-          <button className="theme-color" onClick={() => input("*")}>
-            <FaTimes />
+          <button className="hover" onClick={() => input("*")}>
+            <span className="theme-color">
+              <FaTimes />
+            </span>
           </button>
-          <button onClick={() => input("4")}>
+          <button className="hover" onClick={() => input("4")}>
             <span className="primary-color">4</span>
           </button>
-          <button onClick={() => input("5")}>
+          <button className="hover" onClick={() => input("5")}>
             <span className="primary-color">5</span>
           </button>
-          <button onClick={() => input("6")}>
+          <button className="hover" onClick={() => input("6")}>
             <span className="primary-color">6</span>
           </button>
-          <button className="theme-color" onClick={() => input("-")}>
-            <FaMinus />
+          <button className="hover" onClick={() => input("-")}>
+            <span className="theme-color">
+              <FaMinus />
+            </span>
           </button>
-          <button onClick={() => input("1")}>
+          <button className="hover" onClick={() => input("1")}>
             <span className="primary-color">1</span>
           </button>
-          <button onClick={() => input("2")}>
+          <button className="hover" onClick={() => input("2")}>
             <span className="primary-color">2</span>
           </button>
-          <button onClick={() => input("3")}>
+          <button className="hover" onClick={() => input("3")}>
             <span className="primary-color">3</span>
           </button>
-          <button className="theme-color" onClick={() => input("+")}>
-            <FaPlus />
+          <button className="hover" onClick={() => input("+")}>
+            <span className="theme-color">
+              <FaPlus />
+            </span>
           </button>
-          <button className="no-hover"></button>
-          <button onClick={() => input("0")}>
+          <button className=""></button>
+          <button className="hover" onClick={() => input("0")}>
             <span className="primary-color">0</span>
           </button>
-          <button className="no-hover">
+          <button className="">
             {/* <span className="primary-color">.</span> */}
           </button>
-          <button className="theme-color" onClick={() => showResult()}>
-            <FaEquals />
+          <button className="hover" onClick={() => showResult()}>
+            <span className="theme-color">
+              <FaEquals />
+            </span>
           </button>
         </div>
       </main>
+      {/* <Info /> */}
     </>
-  );
+  )
 }
